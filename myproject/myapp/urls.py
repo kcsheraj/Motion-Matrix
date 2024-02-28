e5b9861e0be217e1f0348781373352b51b28a22f
Sheraj Kc <88295555+kcsheraj@users.noreply.github.com>
2024-02-28 12:30:00 -0500

    Created html,css files for each part of website.

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.landingPage, name="landingPage"),
+    path('loginPage/', views.loginPage, name='loginPage'),
+    path('dashboardPage/', views.dashboardPage, name='dashboardPage'),
+    path('calendarPage/', views.calendarPage, name='calendarPage'),
+    path('workoutRoutinePage/', views.workoutRoutinePage, name='workoutRoutinePage'), 
+    path('prsPage/', views.prsPage, name='prsPage'),
+    path('exploreWorkoutsPage/', views.exploreWorkoutsPage, name='exploreWorkoutsPage'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,5 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world!")
+# Create your views here.
+def landingPage(request):
+    return render(request, "landingPage.html")
+
+def loginPage(request):
+    # Add your view logic here
+    return render(request, 'loginPage.html')
+
+def dashboardPage(request):
+    # Add your view logic here
+    return render(request, 'dashboardPage.html')
+    
+def calendarPage(request):
+    # Add your view logic here
+    return render(request, 'calendarPage.html')
+    
+def workoutRoutinePage(request):
+    # Add your view logic here
+    return render(request, 'workoutRoutinePage.html')
+
+def prsPage(request):
+    # Add your view logic here
+    return render(request, 'prsPage.html')
+
+def exploreWorkoutsPage(request):
+    # Add your view logic here
+    return render(request, 'exploreWorkoutsPage.html')
