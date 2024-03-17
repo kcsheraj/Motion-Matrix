4d65549cef2b2c6160c26d7558d8300dbcc7db51
Sheraj Kc <88295555+kcsheraj@users.noreply.github.com>
2024-03-17 10:24:24 -0400

    update calander script

--- myproject/myapp/static/js/calendarScript.js
+++ myproject/myapp/static/js/calendarScript.js
@@ -1,3 +1,4 @@
+//Updating Calander Script
 const daysTag = document.querySelector(".days"),
   currentDate = document.querySelector(".current-date"),
   prevNextIcon = document.querySelectorAll(".icons span");
